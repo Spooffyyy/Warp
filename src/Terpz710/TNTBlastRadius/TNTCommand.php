5cbd32d172f6c4e78ab503b0aa9f8769ba71ab70
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-09-19 20:13:57 -0700

    Update TNTCommand.php

--- src/Terpz710/TNTBlastRadius/TNTCommand.php
+++ src/Terpz710/TNTBlastRadius/TNTCommand.php
@@ -1,32 +1,28 @@
 <?php
 
-namespace Terpz710;
-
 namespace Terpz710\TNTBlastRadius;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\Plugin;
 use pocketmine\player\Player;
 
 class TNTCommand extends Command {
 
     private $plugin;
 
-    public function __construct(Plugin $plugin) {
-        parent::__construct("tntradius", "Change TNT blast radius");
+    public function __construct(Main $plugin) {
+        parent::__construct("tntradius", "Adjust the TNT blast radius");
+        $this->setPermission("tntradius.command");
+        $this->setAliases(["tntedit"]);
         $this->plugin = $plugin;
-        $this->setPermission("tntradius.command"); 
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if (!$sender instanceof Player) {
-            $sender->sendMessage("§cYou must be in-game to run this command");
-            return true;
+        if ($sender instanceof Player) {
+            $this->plugin->openRadiusSelectorUI($sender); // Call the openRadiusSelectorUI method from the Main class.
+        } else {
+            $sender->sendMessage("This command can only be used in-game.");
         }
-
-        $this->plugin->openRadiusSelectorUI($sender);
-
         return true;
     }
 }
