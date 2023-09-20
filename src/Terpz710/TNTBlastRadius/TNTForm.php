a1290a7af47103a5cb6f36e56c034e82459abccf
Spegatti <145265002+Spegatti@users.noreply.github.com>
2023-09-19 17:05:54 -0700

    Update TNTForm.php

--- src/Terpz710/TNTBlastRadius/TNTForm.php
+++ src/Terpz710/TNTBlastRadius/TNTForm.php
@@ -3,7 +3,6 @@
 namespace Terpz710\TNTBlastRadius;
 
 use pocketmine\event\Listener;
-use pocketmine\event\entity\ExplosionPrimeEvent;
 use pocketmine\entity\object\PrimedTNT;
 use pocketmine\player\Player;
 use jojoe77777\FormAPI\CustomForm;
@@ -11,23 +10,25 @@ use jojoe77777\FormAPI\SimpleForm;
 
 class TNTForm implements Listener {
 
+    private static $blastRadius = 4;
+
     public static function execute(Player $player): void {
         $form = new CustomForm(function (Player $player, ?array $data) {
             if ($data !== null) {
-                $radius = max(1, min(25, (int)$data[0]));
+                self::$blastRadius = max(1, min(25, (int)$data[0]));
 
-                $confirmation = new SimpleForm(function (Player $player, int $data) use ($radius) {
+                $confirmation = new SimpleForm(function (Player $player, int $data) {
                     if ($data === 0) {
-                        
-                        $player->sendMessage("Successfully changed Blast Radius to: " . $radius);
+                       
+                        $player->sendMessage("Successfully changed Blast Radius to: " . self::$blastRadius);
                     } else {
-                        
+                    
                         $player->sendMessage("Blast radius change canceled.");
                     }
                 });
 
                 $confirmation->setTitle("Confirm Radius");
-                $confirmation->setContent("Are you sure you want to set the TNT blast radius to $radius?");
+                $confirmation->setContent("Are you sure you want to set the TNT blast radius to " . self::$blastRadius . "?");
                 $confirmation->addButton("Yes");
                 $confirmation->addButton("No");
 
@@ -36,18 +37,12 @@ class TNTForm implements Listener {
         });
 
         $form->setTitle("TNT Blast Radius");
-        $form->addSlider("Radius:", 1, 25, 1, 1);
+        $form->addSlider("Radius:", 1, 25, 1, self::$blastRadius); // Use the stored radius as the default
         $player->sendForm($form);
     }
 
-    public function onExplosionPrime(ExplosionPrimeEvent $event) {
-        $tnt = $event->getEntity();
-        if ($tnt instanceof PrimedTNT) {
-            $player = $tnt->getOwningEntity();
-            if ($player instanceof Player) {
-                $scaledRadius = max(1, min(25, $tnt->getRadius()));
-                $event->setRadius($scaledRadius);
-            }
-        }
+    public function onTNTIgnite(Player $player, PrimedTNT $tnt): void {
+        $scaledRadius = max(1, min(25, self::$blastRadius));
+        $tnt->setRadius($scaledRadius);
     }
 }
